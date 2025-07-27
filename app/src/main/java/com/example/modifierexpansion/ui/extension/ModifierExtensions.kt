4a38653d1d1ba9e439efeb8229055d170790c869
choimingyu <alsrb1315093@gmail.com>
2025-07-28 07:55:02 +0900

    Modifier 확장 함수

--- app/src/main/java/com/example/modifierexpansion/ui/extension/ModifierExtensions.kt
+++ app/src/main/java/com/example/modifierexpansion/ui/extension/ModifierExtensions.kt
@@ -3,22 +3,22 @@ package com.example.modifierexpansion.ui.extension
 import androidx.compose.ui.Modifier
 
 class ConditionalModifierBuilder(
-    private val base: Modifier,
+    private val modifier: Modifier,
     private val condition: Boolean
 ) {
-    private var trueModifier: Modifier = base
-    private var falseModifier: Modifier = base
+    private var trueModifier: Modifier = modifier
+    private var falseModifier: Modifier = modifier
 
     fun ifTrue(block: Modifier.() -> Modifier): ConditionalModifierBuilder {
         if (condition) {
-            trueModifier = base.block()
+            trueModifier = modifier.block()
         }
         return this
     }
 
     fun ifFalse(block: Modifier.() -> Modifier): Modifier {
         if (!condition) {
-            falseModifier = base.block()
+            falseModifier = modifier.block()
         }
         return if (condition) trueModifier else falseModifier
     }
