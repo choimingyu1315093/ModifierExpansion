e0eed56010350283b1730430ca2f5faaed9c3113
choimingyu <alsrb1315093@gmail.com>
2025-07-28 20:53:34 +0900

    Modifier 확장 함수

--- app/src/main/java/com/example/modifierexpansion/MainActivity.kt
+++ app/src/main/java/com/example/modifierexpansion/MainActivity.kt
@@ -4,30 +4,22 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.modifierexpansion.ui.extension.conditional
-import com.example.modifierexpansion.ui.theme.ModifierExpansionTheme
+
 
 data class Student(
     val name: String,
@@ -40,13 +32,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             val students = listOf(
-                Student("김학생", "20", true),
-                Student("박학생", "21", false),
-                Student("이학생", "22", true),
-                Student("최학생", "23", true)
+                Student("A", "20", true),
+                Student("B", "20", false),
+                Student("C", "20", true),
+                Student("D", "20", true)
             )
 
-            StudentCard(students = students)
+            StudentCard(students)
         }
     }
 }
@@ -54,6 +46,7 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun StudentCard(students: List<Student>){
     val context = LocalContext.current
+
     LazyColumn {
         items(students){ student ->
             Row (
@@ -61,52 +54,24 @@ fun StudentCard(students: List<Student>){
                     .conditional(student.isChecked)
                     .ifTrue {
                         background(Color.Cyan)
-                        .border(BorderStroke(0.dp, Color.Transparent))
-                        .padding(30.dp)
-                        .clickable {
-                            Toast.makeText(context, "true", Toast.LENGTH_SHORT).show()
-                        }
+                            .border(BorderStroke(0.dp, Color.Transparent))
+                            .padding(30.dp)
+                            .clickable {
+                                Toast.makeText(context, "True", Toast.LENGTH_SHORT).show()
+                            }
                     }
                     .ifFalse {
                         background(Color.Red.copy(alpha = .5f))
-                        .border(BorderStroke(2.dp, Color.Black))
-                        .padding(50.dp)
-                        .clickable {
-                            Toast.makeText(context, "false", Toast.LENGTH_SHORT).show()
-                        }
+                            .border(BorderStroke(2.dp, Color.Black))
+                            .padding(50.dp)
+                            .clickable {
+                                Toast.makeText(context, "False", Toast.LENGTH_SHORT).show()
+                            }
                     }
-//                    .background(
-//                        if(student.isChecked){
-//                            Color.Cyan
-//                        }else {
-//                            Color.Red.copy(alpha = 0.5f)
-//                        }
-//                    )
-//                    .border(
-//                        if(student.isChecked){
-//                            BorderStroke(0.dp, Color.Transparent)
-//                        }else {
-//                            BorderStroke(2.dp, Color.Black)
-//                        }
-//                    )
-//                    .padding(
-//                        if(student.isChecked){
-//                            30.dp
-//                        }else {
-//                            50.dp
-//                        }
-//                    )
-//                    .clickable {
-//                        if(student.isChecked){
-//                            Toast.makeText(context, "true", Toast.LENGTH_SHORT).show()
-//                        }else {
-//                            Toast.makeText(context, "false", Toast.LENGTH_SHORT).show()
-//                        }
-//                    }
             ){
                 Text(
                     modifier = Modifier.weight(1f),
-                    text = student.name,
+                    text = student.name
                 )
                 Text(
                     modifier = Modifier.weight(1f),
@@ -116,4 +81,3 @@ fun StudentCard(students: List<Student>){
         }
     }
 }
-
--- app/src/main/java/com/example/modifierexpansion/ui/extension/ModifierExtensions.kt
+++ app/src/main/java/com/example/modifierexpansion/ui/extension/ModifierExtensions.kt
@@ -5,22 +5,22 @@ import androidx.compose.ui.Modifier
 class ConditionalModifierBuilder(
     private val modifier: Modifier,
     private val condition: Boolean
-) {
+){
     private var trueModifier: Modifier = modifier
     private var falseModifier: Modifier = modifier
 
     fun ifTrue(block: Modifier.() -> Modifier): ConditionalModifierBuilder {
-        if (condition) {
+        if(condition){
             trueModifier = modifier.block()
         }
         return this
     }
 
     fun ifFalse(block: Modifier.() -> Modifier): Modifier {
-        if (!condition) {
+        if(!condition){
             falseModifier = modifier.block()
         }
-        return if (condition) trueModifier else falseModifier
+        return if(condition) trueModifier else falseModifier
     }
 }
 
